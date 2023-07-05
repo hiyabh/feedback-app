86cdd67c9507c2e8e45b562cd71e2bd70fbe2dca
hiya <34233282+hiyabh@users.noreply.github.com>
2023-07-05 16:13:10 +0300

    change data to arrive from db.json

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,31 +1,29 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
-    const [feedback, setFeedback] = useState([
-      {
-        id: 1,
-        rating: 10,
-        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.",
-      },
-      {
-        id: 2,
-        rating: 9,
-        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.",
-      },
-      {
-        id: 3,
-        rating: 8,
-        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.",
-      },
-    ]);
+  const [isLoading, setIsLoading] = useState(true);
+    const [feedback, setFeedback] = useState([]);
 
     const [feedbackEdit, setFeedbackEdit] = useState({
       item: {},
       edit: false,
     });
+
+    useEffect(() => {
+          fetchFeedback();
+        }, []);
+    
+    const fetchFeedback = async () => {
+      const response = await fetch("http://localhost:5000/feedback?_sort=id&_order=desc")
+      const data = await response.json();
+      setFeedback(data);
+      setIsLoading(false);
+    };
+
+    
 
     const addFeedback = (newFeedback) => {
     newFeedback.id = uuidv4();
@@ -55,6 +53,7 @@ export const FeedbackProvider = ({ children }) => {
         value={{
           feedback,
           feedbackEdit,
+          isLoading,
           deleteFeedback,
           addFeedback,
           editFeedback,
