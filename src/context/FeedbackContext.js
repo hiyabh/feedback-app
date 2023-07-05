f8eeb5f2f681df4df1379f8742d399605fc43da5
hiya <34233282+hiyabh@users.noreply.github.com>
2023-07-05 17:37:49 +0300

    All Api work!

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -36,14 +36,27 @@ export const FeedbackProvider = ({ children }) => {
       setFeedback([data, ...feedback]);
     };
 
-    const deleteFeedback = (id) => {
+    const deleteFeedback = async (id) => {
       if (window.confirm("Are you sure you want to delete this feedback?")) {
+        await fetch(`/feedback/${id}`, {
+          method: "DELETE"});
+
         setFeedback(feedback.filter((feedback) => feedback.id !== id));
       }
     };
 
-    const updateFeedback = (id, updItem) => {
-      setFeedback(feedback.map((item) => item.id === id ? {...item, ...updItem} : item))
+    const updateFeedback = async (id, updItem) => {
+      const response = await fetch(`/feedback/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(updItem)
+      });
+      const data = await response.json();
+      setFeedback(
+        feedback.map((item) => (item.id === id ? { ...item, ...data} : item))
+      );
     };
 
     const editFeedback = (item) => {
