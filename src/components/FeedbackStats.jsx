c5a9f8310f3de310eb31a6c24939487c045cf696
hiya <34233282+hiyabh@users.noreply.github.com>
2023-07-05 17:57:26 +0300

    Fix the average

--- src/components/FeedbackStats.jsx
+++ src/components/FeedbackStats.jsx
@@ -2,19 +2,18 @@ import { useContext } from "react";
 import FeedbackContext from "../context/FeedbackContext";
 
 function FeedbackStats() {
-   const { feedback } = useContext(FeedbackContext);
+  const { feedback } = useContext(FeedbackContext);
+  console.log('feedback: ', typeof(feedback.rating));
 
-  let average =
-    feedback.reduce((acc, cur) => {
-      return acc + cur.rating;
-    }, 0) / feedback.length;
-
-  average = average.toFixed(1).replace(/[.,]0$/, "");
+  const average =
+    feedback.length === 0
+      ? 0
+      : feedback.reduce((acc, { rating }) => acc + rating, 0) / feedback.length;
 
   return (
     <div className="feedback-stats">
       <h4>{feedback.length} Reviews</h4>
-      <h4>Average Rating: {isNaN(average) ? 0 : average}</h4>
+      <h4>Average Rating: {average.toFixed(1).replace(/[.,]0$/, "")}</h4>
     </div>
   );
 }
