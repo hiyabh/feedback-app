fa6583e58c095e75b34a40980fcd5c29003f7d73
hiya <34233282+hiyabh@users.noreply.github.com>
2023-07-05 17:29:39 +0300

    Add with json-server work

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,5 +1,4 @@
 import { createContext, useState, useEffect } from "react";
-import { v4 as uuidv4 } from "uuid";
 
 const FeedbackContext = createContext();
 
@@ -17,7 +16,7 @@ export const FeedbackProvider = ({ children }) => {
         }, []);
     
     const fetchFeedback = async () => {
-      const response = await fetch("http://localhost:5000/feedback?_sort=id&_order=desc")
+      const response = await fetch("/feedback?_sort=id&_order=desc")
       const data = await response.json();
       setFeedback(data);
       setIsLoading(false);
@@ -25,9 +24,16 @@ export const FeedbackProvider = ({ children }) => {
 
     
 
-    const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4();
-    setFeedback([newFeedback, ...feedback]);
+    const addFeedback = async (newFeedback) => {
+      const response = await fetch("/feedback", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newFeedback),
+      });
+      const data = await response.json();
+      setFeedback([data, ...feedback]);
     };
 
     const deleteFeedback = (id) => {
